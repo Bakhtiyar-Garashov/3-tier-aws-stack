7de82280b5cbbead59c94611b86de21569e0903e
Bakhtiyar-Garashov <bakhtiyar.garashov@gmail.com>
2024-07-31 22:49:02 +0300

    Reorganize outputs

--- iac/envs/staging/main.tf
+++ iac/envs/staging/main.tf
@@ -13,18 +13,3 @@ module "vpc" {
     Environment = "staging"
   }
 }
-
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-output "public_subnets" {
-  description = "The IDs of the public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "private_subnets" {
-  description = "The IDs of the private subnets"
-  value       = module.vpc.private_subnets
-}
